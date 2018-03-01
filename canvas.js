d48fdb3c4aaa20ca316e2c514920fc0160cdc5a4
Angelica Zverovich <azverovich@stuy.edu>
2018-03-01 14:00:47 -0500

    commit

--- canvas.js
+++ canvas.js
@@ -4,12 +4,20 @@ var toggle = document.getElementById("tb");
 var canvas = document.getElementById("slate");
 var ctx = canvas.getContext("2d");
 var mode = "circle";
+var lastX = null;
+var lastY = null;
 
 canvas.onclick = function(e){
     if (mode == "square") ctx.fillRect(e.offsetX, e.offsetY, 20,20);
     else {
       ctx.beginPath();
       ctx.arc(e.offsetX, e.offsetY, 9, 0 , 2 * Math.PI);
+     if (lastX != null && lastY != null){
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+    }
+    lastX = e.offsetX;
+    lastY = e.offsetY;
       ctx.stroke();
       ctx.fill();
     }
